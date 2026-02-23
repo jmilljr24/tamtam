51e20547dcee092aa1fe68fa2fc4ab55fc15d224
Justin Miller <16829344+jmilljr24@users.noreply.github.com>
2026-02-23 12:44:04 -0500

    update kamal

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.1].define(version: 2026_02_22_010447) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_catalog.plpgsql"
-
   create_table "homes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
